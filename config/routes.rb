acdca8262a66006e652968daadf29710695949ce
Johnathan Smith <smithjohnathanr@gmail.com>
2015-05-02 12:36:55 -0700

    Added controller and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
   resources :contacts
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
